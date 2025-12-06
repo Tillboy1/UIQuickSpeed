8c2552ba92925734042b0997f71c1708fe323a10
Tillboy1 <keiranm9@gmail.com>
2025-12-06 12:10:38 +0000

    UI compleate need scripts

--- Assets/GameplaySettings.cs
+++ Assets/GameplaySettings.cs
@@ -2,9 +2,20 @@ using UnityEngine;
 
 public class GameplaySettings : MonoBehaviour
 {
-    public bool PuzzleAimAssist;
-    public bool ;
-    public bool PuzzleAimAssist;
-    public bool PuzzleAimAssist;
-    public bool PuzzleAimAssist;
+    public int difficulty;
+    public bool puzzleAimAssist;
+    public int puzzleTiming;
+    public bool lockOnCamera;
+    public bool resetCameraOnAttack;
+    public bool strafeAssist;
+
+    public bool autoPickUp;
+    public bool menuHolders;
+    public bool mini_GameStyle;
+    public bool navigationAssist;
+    public bool RepeatedButtonPresses;
+    public bool SkillsNavigation;
+    public bool traversalAssist;
+    public bool tutorialAssist;
+    public bool weaponSheathing;
 }
