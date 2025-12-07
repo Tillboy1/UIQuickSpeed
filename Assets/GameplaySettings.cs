968e1473b0dad95c22cacf9dc9a5f2f8de1f358c
Tillboy1 <keiranm9@gmail.com>
2025-12-07 00:06:38 +0000

    Finishing Base Of Gameplaysettings + UI
    might remove the hold button down as I think there are two of those also might remove another due to being able to put it in controls area

--- Assets/GameplaySettings.cs
+++ Assets/GameplaySettings.cs
@@ -1,21 +1,232 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GameplaySettings : MonoBehaviour
 {
-    public int difficulty;
+    // This would be removed in the build of the game for performance but good to test to find optimum game balance
+    [Header("Edditor")]
+    public float difficultyEasySet;
+
+    // Holds all of our data :D
+    [Header("GamePlay")]
+    public float difficulty;
     public bool puzzleAimAssist;
-    public int puzzleTiming;
-    public bool lockOnCamera;
-    public bool resetCameraOnAttack;
+    public float puzzleTiming;
+    public int lockOnCamera;
+    public int resetCameraOnAttack;
     public bool strafeAssist;
 
-    public bool autoPickUp;
-    public bool menuHolders;
-    public bool mini_GameStyle;
+    [Header("PlayStyle")]
+    public int autoPickUp;
+    public int menuHolders;
+    public int mini_GameStyle;
     public bool navigationAssist;
     public bool RepeatedButtonPresses;
     public bool SkillsNavigation;
-    public bool traversalAssist;
+    public int traversalAssist;
     public bool tutorialAssist;
     public bool weaponSheathing;
+
+    #region
+    public void SetDifficulty(int dropInput)
+    {
+        switch (dropInput)
+        {
+            case 0:
+                difficulty = difficultyEasySet;
+                PlayerPrefs.SetFloat("difficultyFloat", difficulty);
+                PlayerPrefs.Save();
+                break;
+            default:
+                difficulty = dropInput;
+                PlayerPrefs.SetFloat("difficultyFloat", difficulty);
+                PlayerPrefs.Save();
+                break;
+        }
+    }
+    public void SetPuzzleAimAssist(int dropInput)
+    {
+        switch (dropInput)
+        {
+            case 0:
+                puzzleAimAssist = false;
+                PlayerPrefs.SetInt("puzzleAimAssistBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+            default:
+                puzzleAimAssist = true;
+                PlayerPrefs.SetInt("puzzleAimAssistBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+        }
+    }
+    public void SetPuzzleTiming(int dropInput)
+    {
+        switch (dropInput)
+        {
+            case 0:
+                puzzleTiming = difficultyEasySet;
+                PlayerPrefs.SetFloat("puzzleTimeFloat", dropInput);
+                PlayerPrefs.Save();
+                break;
+            default:
+                puzzleTiming = dropInput;
+                PlayerPrefs.SetFloat("puzzleTimeFloat", dropInput);
+                PlayerPrefs.Save();
+                break;
+        }
+    }
+    public void SetLockOnCamera(int dropInput)
+    {
+        lockOnCamera = dropInput;
+        PlayerPrefs.SetInt("resetCamOnAttackInt", dropInput);
+        PlayerPrefs.Save();
+    }
+    public void SetResetCamOnAttack(int dropInput)
+    {
+        resetCameraOnAttack = dropInput;
+        PlayerPrefs.SetInt("resetCamOnAttackInt", dropInput);
+        PlayerPrefs.Save();
+    }
+    public void SetStrafeAssist(int dropInput)
+    {
+        switch (dropInput)
+        {
+            case 0:
+                strafeAssist = false;
+                PlayerPrefs.SetFloat("strafeAssistBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+            default:
+                strafeAssist = true;
+                PlayerPrefs.SetFloat("strafeAssistBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+        }
+    }
+    #endregion
+    #region
+    public void SetAutoPickup(int dropInput)
+    {
+        autoPickUp = dropInput;
+        PlayerPrefs.SetInt("autoPickUpInt", dropInput);
+        PlayerPrefs.Save();
+    }
+    public void SetMenuHold(int dropInput)
+    {
+        menuHolders = dropInput;
+        PlayerPrefs.SetInt("menuHoldInt", dropInput);
+        PlayerPrefs.Save();
+    }
+    public void SetMiniGameStyle(int dropInput)
+    {
+        mini_GameStyle = dropInput;
+        PlayerPrefs.SetInt("miniGameStyleInt", dropInput);
+        PlayerPrefs.Save();
+    }
+    public void SetNavigationAssist(int dropInput)
+    {
+        switch (dropInput)
+        {
+            case 0:
+                navigationAssist = false;
+                PlayerPrefs.SetFloat("strafeAssistBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+            default:
+                navigationAssist = true;
+                PlayerPrefs.SetFloat("strafeAssistBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+        }
+    }
+    public void SetRepeatedButtonPress(int dropInput)
+    {
+        switch (dropInput)
+        {
+            case 0:
+                RepeatedButtonPresses = false;
+                PlayerPrefs.SetFloat("repeatedButtonPressBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+            default:
+                RepeatedButtonPresses = true;
+                PlayerPrefs.SetFloat("repeatedButtonPressBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+        }
+    }
+    public void SetSkillsNavigation(int dropInput)
+    {
+        switch (dropInput)
+        {
+            case 0:
+                SkillsNavigation = false;
+                PlayerPrefs.SetFloat("skillsNavigationBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+            default:
+                SkillsNavigation = true;
+                PlayerPrefs.SetFloat("skillsNavigationBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+        }
+    }
+    public void SetTraversalAssist(int dropInput)
+    {
+        traversalAssist = dropInput;
+        PlayerPrefs.SetInt("traversalAssistInt", dropInput);
+        PlayerPrefs.Save();
+    }
+    public void SetTutorial(int dropInput)
+    {
+        switch (dropInput)
+        {
+            case 0:
+                tutorialAssist = false;
+                PlayerPrefs.SetInt("tutorialAssistBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+            default:
+                tutorialAssist = true;
+                PlayerPrefs.SetInt("tutorialAssistBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+        }
+    }
+    public void SetWeaponSheathing(int dropInput)
+    {
+        switch (dropInput)
+        {
+            case 0:
+                weaponSheathing = false;
+                PlayerPrefs.SetInt("weaponSheathingBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+            default:
+                weaponSheathing = true;
+                PlayerPrefs.SetInt("weaponSheathingBool", dropInput);
+                PlayerPrefs.Save();
+                break;
+        }
+    }
+    #endregion
+    /*
+    public void SetDifficulty(int dropInput)
+    {
+        switch (dropInput)
+        {
+            case 0:
+                difficulty = difficultyEasySet;
+                PlayerPrefs.SetFloat("difficultyFloat", dropInput);
+                PlayerPrefs.Save();
+                break;
+            default:
+                difficulty = dropInput;
+                PlayerPrefs.SetFloat("difficultyFloat", dropInput);
+                PlayerPrefs.Save();
+                break;
+        }
+    }
+    */
 }
